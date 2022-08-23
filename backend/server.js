7e8b34a50815b202d53fa1ffa95207bc4d6ecb76
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-08-23 15:49:15 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const { readdirSync } = require("fs");
 const app = express();
 app.use(cors());
 
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
-app.listen(8000, () => {
-	console.log("Server is listening");
+PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
 });
