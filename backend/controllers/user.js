bb978e6a49c65bf15142490da40840931fcc1bb7
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-08-23 17:45:57 +0530

    register and validate data using regex to validate

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,6 @@
+const { validateEmail, validateLength } = require("../helpers/validation");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 exports.register = async (req, res) => {
 	try {
 		const {
@@ -12,6 +14,40 @@ exports.register = async (req, res) => {
 			bDay,
 			gender,
 		} = req.body;
+
+		if (!validateEmail(email)) {
+			return res.status(400).json({
+				message: "Invalid e-mail address",
+			});
+		}
+
+		const check = await User.findOne({ email });
+		if (check) {
+			return res.status(400).json({
+				message:
+					"This email address already exists, try with a different email address",
+			});
+		}
+
+		if (!validateLength(first_name, 3, 30)) {
+			return res.status(400).json({
+				message: "First name must be between 3 and 30 characters.",
+			});
+		}
+
+		if (!validateLength(last_name, 3, 30)) {
+			return res.status(400).json({
+				message: "Last name must be between 3 and 30 characters.",
+			});
+		}
+
+		if (!validateLength(password, 6, 30)) {
+			return res.status(400).json({
+				message: "Password must be at least 6 characters.",
+			});
+		}
+		const cryptedPassword = await bcrypt.hash(password, 12);
+		return;
 
 		const user = await new User({
 			first_name,
---
+++ backend/helpers/validation.js
@@ -0,0 +1,10 @@
+exports.validateEmail = (email) => {
+	return String(email)
+		.toLocaleLowerCase()
+		.match(/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,12})(\.[a-z]{2,12})?$/);
+};
+
+exports.validateLength = (text, min, max) => {
+	if (text.length > max || text.length < min) return false;
+	return true;
+};
