f3738ab52faac94cb23d944629072166e124e66d
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-08-23 16:07:53 +0530

    connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const { readdirSync } = require("fs");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const { readdirSync } = require("fs");
 const app = express();
 app.use(cors());
 
+// routes
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
-PORT = process.env.PORT || 8000
+// database
+mongoose
+	.connect(process.env.DATABASE_URL, {
+		useNewUrlParser: true,
+	})
+	.then(() => console.log("Database connected successfully"))
+	.catch((err) => console.log("Error connecting to db", err));
+
+PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
 });
