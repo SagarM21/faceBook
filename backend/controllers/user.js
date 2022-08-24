6c48caa3bbc3663f0b1620436449799dac936650
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-08-24 17:52:11 +0530

    Activate email

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -5,6 +5,7 @@ const {
 } = require("../helpers/validation");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { generateToken } = require("../helpers/tokens");
 const { sendVerificationEmail } = require("../helpers/mailer");
 exports.register = async (req, res) => {
@@ -86,5 +87,19 @@ exports.register = async (req, res) => {
 		});
 	} catch (error) {
 		res.status(500).json({ message: error.message });
+	}
+};
+
+exports.activateAccount = async (req, res) => {
+	const { token } = req.body;
+	const user = jwt.verify(token, process.env.TOKEN_SECRET);
+	const check = await User.findById(user.id);
+	if (check.verified == true) {
+		return res.status(400).json({ message: "This email is already activated" });
+	} else {
+		await User.findByIdAndUpdate(user.id, { verified: true });
+		return res
+			.status(200)
+			.json({ message: "Account has been activated successfully!" });
 	}
 };
