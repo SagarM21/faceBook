811d29f3da146e69ceb7d90ef6707516a0686a6b
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-08-29 19:55:45 +0530

    register form mobile

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,6 +1,6 @@
-
 import Footer from "../../components/login/Footer";
 import LoginForm from "../../components/login/LoginForm";
+import RegisterForm from "../../components/login/RegisterForm";
 import "./style.css";
 
 export default function Login() {
@@ -8,7 +8,7 @@ export default function Login() {
 		<div className='login'>
 			<div className='login_wrapper'>
 				<LoginForm />
-				<div className='register'></div>
+				<RegisterForm />
 				<Footer />
 			</div>
 		</div>
