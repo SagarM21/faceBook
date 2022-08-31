c83f866ad241691af5f7af60e78dcd64070febea
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-08-31 21:53:57 +0530

    header half ready

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "../../components/header";
 
 const Home = () => {
 	return (
-		<div className='font-bold text-black flex justify-center items-center text-5xl'>
-			Home
+		<div className=''>
+			<Header />
 		</div>
 	);
 };
