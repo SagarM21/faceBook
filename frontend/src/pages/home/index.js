1b364d454fc9cd1b4be9867aaf6006a6967174d1
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-01 14:56:46 +0530

    click outside func

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Header from "../../components/header";
+import useClickOutside from "../../helpers/clickOutside";
 
 const Home = () => {
+	const [visible, setVisible] = useState(false);
+	const el = useRef(null);
+	useClickOutside(el, () => {
+		setVisible(false);
+	});
 	return (
 		<div className=''>
 			<Header />
+			{visible && <div className='card' ref={el}></div>}
 		</div>
 	);
 };
