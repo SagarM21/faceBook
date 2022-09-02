d67bb15e92860464451a25edd683a5dae29e4108
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-02 21:02:06 +0530

    changes

--- frontend/src/components/header/SearchMenu.js
+++ frontend/src/components/header/SearchMenu.js
@@ -1,20 +1,16 @@
 import { useEffect, useRef, useState } from "react";
 import { Return, Search } from "../../svg";
 import useClickOutside from "../../helpers/clickOutside";
-
 export default function SearchMenu({ color, setShowSearchMenu }) {
+	const [iconVisible, setIconVisible] = useState(true);
 	const menu = useRef(null);
 	const input = useRef(null);
 	useClickOutside(menu, () => {
 		setShowSearchMenu(false);
 	});
-
-	const [iconVisible, setIconVisible] = useState(false);
-
 	useEffect(() => {
 		input.current.focus();
 	}, []);
-
 	return (
 		<div className='header_left search_area scrollbar' ref={menu}>
 			<div className='search_wrap'>
