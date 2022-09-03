a71718777874ee63cae75f5ae06d05f9a15c828c
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-03 14:40:02 +0530

    left side of home half done

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,17 +1,13 @@
-import React, { useRef, useState } from "react";
+import { useSelector } from "react-redux";
 import Header from "../../components/header";
-import useClickOutside from "../../helpers/clickOutside";
+import LeftHome from "../../components/home/left";
 
 const Home = () => {
-	const [visible, setVisible] = useState(false);
-	const el = useRef(null);
-	useClickOutside(el, () => {
-		setVisible(false);
-	});
+	const { user } = useSelector((user) => ({ ...user }));
 	return (
-		<div className=''>
+		<div>
 			<Header />
-			{visible && <div className='card' ref={el}></div>}
+			<LeftHome user={user} />
 		</div>
 	);
 };
