b7e93e746dbabd448a6eec4e1708e46944547c25
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-03 14:25:47 +0530

    protected routes setup for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
+import LoggedInRoute from "./routes/LoggedInRoute";
+import NotLoggedInRoute from "./routes/NotLoggedInRoute";
 
 const App = () => {
 	return (
 		<Routes>
-			<Route path='/' element={<Home />} exact />
-			<Route path='/login' element={<Login />} exact />
-			<Route path='/profile' element={<Profile />} exact />
+			<Route element={<LoggedInRoute />}>
+				<Route path='/' element={<Home />} exact />
+				<Route path='/profile' element={<Profile />} exact />
+			</Route>
+			<Route element={<NotLoggedInRoute />}>
+				<Route path='/login' element={<Login />} exact />
+			</Route>
 		</Routes>
 	);
 };
