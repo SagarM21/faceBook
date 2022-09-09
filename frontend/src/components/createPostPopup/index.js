3c0364eded26a0420894165290a476e08d98d83b
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-09 12:52:16 +0530

    emoji picker/frontend -- createPost

--- frontend/src/components/createPostPopup/index.js
+++ frontend/src/components/createPostPopup/index.js
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./style.css";
-
+import Picker from "emoji-picker-react";
 export default function CreatePostPopup({ user }) {
 	const [text, setText] = useState("");
 	const [showPrev, setShowPrev] = useState(false);
+	const [picker, setPicker] = useState(false);
+	const textRef = useRef(null);
+	const [cursorPosition, setCursorPosition] = useState();
+	useEffect(() => {
+		textRef.current.selectionEnd = cursorPosition;
+	}, [cursorPosition]);
+	const handleEmoji = (e, { emoji }) => {
+		const ref = textRef.current;
+		ref.focus();
+		const start = text.substring(0, ref.selectionStart);
+		const end = text.substring(ref.selectionStart);
+		const newText = start + emoji + end;
+		setText(newText);
+		setCursorPosition(start.length + emoji.length);
+	};
+	// console.log(text);
 	return (
 		<div className='blur'>
 			<div className='postBox'>
@@ -14,10 +30,10 @@ export default function CreatePostPopup({ user }) {
 					<span>Create Post</span>
 				</div>
 				<div className='box_profile'>
-					<img src={user.picture} alt='' className='box_profile_img' />
+					<img src={user?.picture} alt='' className='box_profile_img' />
 					<div className='box_col'>
 						<div className='box_profile_name'>
-							{user.first_name} {user.last_name}
+							{user?.first_name} {user?.last_name}
 						</div>
 						<div className='box_privacy'>
 							<img src='../../../icons/public.png' alt='' />
@@ -30,16 +46,28 @@ export default function CreatePostPopup({ user }) {
 				{!showPrev && (
 					<div className='flex_center'>
 						<textarea
-							maxlength='100'
+							ref={textRef}
+							maxLength='100'
 							value={text}
-							placeholder={`What's on your mind, ${user.first_name}`}
+							placeholder={`What's on your mind, ${user?.first_name}`}
 							className='post_input'
 							onChange={(e) => setText(e.target.value)}
 						></textarea>
 					</div>
 				)}
 				<div className='post_emojis_wrap'>
-					<div className='comment_emoji_picker rlmove'></div>
+					{picker && (
+						<div className='comment_emoji_picker rlmove'>
+							<Picker onEmojiClick={handleEmoji} />
+						</div>
+					)}
+					<img src='../../../icons/colorful.png' alt='' />
+					<i
+						className='emoji_icon_large'
+						onClick={() => {
+							setPicker((prev) => !prev);
+						}}
+					></i>
 				</div>
 			</div>
 		</div>
