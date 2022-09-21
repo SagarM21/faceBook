601db557d655440b0820901deac5bfb094ab775c
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-21 21:49:33 +0530

    get profile info and images from cloudinary

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -246,3 +246,15 @@ exports.changePassword = async (req, res) => {
 		res.status(500).json({ message: error.message });
 	}
 };
+
+// PROFILE SECTION
+
+exports.getProfile = async (req, res) => {
+	try {
+		const { username } = req.params;
+		const profile = await User.find({ username }).select("-password");
+		res.json(profile);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
