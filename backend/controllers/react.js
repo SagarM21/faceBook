6893948c422346a84aace9cb31afd1bb822fa69f
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-10-08 19:47:23 +0530

    models, controllers and routes for reactions

---
+++ backend/controllers/react.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const React = require("../models/React");
+
+exports.reactPost = async (req, res) => {
+	try {
+		const { postId, react } = req.body;
+		const check = await React.findOne({
+			postRef: postId,
+			reactBy: mongoose.Types.ObjectId(req.user.id),
+		});
+
+		if (check == null) {
+			const newReact = new React({
+				react: react,
+				postRef: postId,
+				reactBy: req.user.id,
+			});
+			await newReact.save();
+		} else {
+			// checking the react which is saved in db is different from the react which are we getting from frontend, the req.body one
+			if (check.react == react) {
+				await React.findByIdAndRemove(check._id);
+			} else {
+				// if not then user is trying to update his reaction
+				await React.findByIdAndUpdate(check._id, { react: react });
+			}
+		}
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	}
+};
